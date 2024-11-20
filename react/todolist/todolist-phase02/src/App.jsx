73b99fe96cf1ffda5318a20c08586840f70aa745
jjori <jjori.master@gmail.com>
2024-11-20 10:19:57 +0900

    React TodoList 2단계 초기화

--- react/todolist/todolist-phase02/src/App.jsx
+++ react/todolist/todolist-phase02/src/App.jsx
@@ -1,54 +1,35 @@
 import './App.css';
-import { useRef, useReducer } from 'react';
+import { useState, useRef } from 'react';
 
 import Header from './components/Header';
 import List from './components/List';
 import Editor from './components/Editor';
 
-const reducer = (state, action) => {
-  switch (action.type) {
-    case 'CREATE':
-      return [action.data, ...state];
-    case 'UPDATE':
-      return state.map(todo =>
-        todo.id === action.targetId ? { ...todo, isDone: !todo.isDone } : todo
-      );
-    case 'DELETE':
-      return state.filter(todo => todo.id !== action.targetId);
-    default:
-      return state;
-  }
-};
-
 function App() {
-  const [todos, dispatch] = useReducer(reducer, []);
-
+  const [todos, setTodos] = useState([]);
   const idRef = useRef(0);
 
   const onCreate = content => {
-    dispatch({
-      type: 'CREATE',
-      data: {
-        id: idRef.current++,
-        isDone: false,
-        content,
-        date: new Date().getTime(),
-      },
-    });
+    const newTodo = {
+      id: idRef.current++,
+      isDone: false,
+      content,
+      date: new Date().getTime(),
+    };
+
+    setTodos([newTodo, ...todos]);
   };
 
   const onUpdate = targetId => {
-    dispatch({
-      type: 'UPDATE',
-      targetId,
-    });
+    setTodos(
+      todos.map(todo =>
+        todo.id === targetId ? { ...todo, isDone: !todo.isDone } : todo
+      )
+    );
   };
 
   const onDelete = targetId => {
-    dispatch({
-      type: 'DELETE',
-      targetId,
-    });
+    setTodos(todos.filter(todo => todo.id !== targetId));
   };
 
   return (
