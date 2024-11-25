88887ce2ecb1a28d5e8d07d3bfb81a2511857f59
jjori <jjori.master@gmail.com>
2024-11-25 23:57:24 +0900

    일기 정렬 기능 추가 및 새 일기 쓰기 페이지 연결

--- react/emotion-diary/emotion-diary-phase01/src/ components/DiaryList.jsx
+++ react/emotion-diary/emotion-diary-phase01/src/ components/DiaryList.jsx
@@ -1,19 +1,44 @@
 import './DiaryList.css';
 import Button from './Button';
 import DiaryItem from './DiaryItem';
+import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 
 const DiaryList = ({ data }) => {
+  const nav = useNavigate();
+  const [sortType, setSortType] = useState('latest');
+
+  const onChangeSortType = e => {
+    setSortType(e.target.value);
+  };
+
+  const getSortedData = () => {
+    return data.toSorted((a, b) => {
+      if (sortType === 'oldest') {
+        return Number(a.createdDate) - Number(b.createdDate);
+      } else {
+        return Number(b.createdDate) - Number(a.createdDate);
+      }
+    });
+  };
+
+  const sortedData = getSortedData();
+
   return (
     <div className="DiaryList">
       <div className="menu_bar">
-        <select>
+        <select onChange={onChangeSortType}>
           <option value={'latest'}>최신순</option>
           <option value={'oldest'}>오래된 순</option>
         </select>
-        <Button text={'새 일기 쓰기'} type={'POSITIVE'} />
+        <Button
+          text={'새 일기 쓰기'}
+          type={'POSITIVE'}
+          onClick={() => nav('/new')}
+        />
       </div>
       <div className="list_wrapper">
-        {data.map(item => (
+        {sortedData.map(item => (
           <DiaryItem key={item.id} {...item} />
         ))}
       </div>
