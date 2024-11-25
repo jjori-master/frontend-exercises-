c40e822d7515d473a1691107ed0e4fda398be579
jjori <jjori.master@gmail.com>
2024-11-25 23:25:20 +0900

    날짜 이동 로직 작성

--- react/emotion-diary/emotion-diary-phase01/src/pages/Home.jsx
+++ react/emotion-diary/emotion-diary-phase01/src/pages/Home.jsx
@@ -1,14 +1,25 @@
+import { useState } from 'react';
+
 import Header from '../ components/Header';
 import Button from '../ components/Button';
 import DiaryList from '../ components/DiaryList';
 
 const Home = () => {
+  const [pivotDate, setPivotDate] = useState(new Date());
+
+  const onIncreaseMonth = () => {
+    setPivotDate(new Date(pivotDate.getFullYear(), pivotDate.getMonth() + 1));
+  };
+  const onDecreaseMonth = () => {
+    setPivotDate(new Date(pivotDate.getFullYear(), pivotDate.getMonth() - 1));
+  };
+
   return (
     <div>
       <Header
-        title={'2024년 11월'}
-        leftChild={<Button text={'<'} />}
-        rightChild={<Button text={'>'} />}
+        title={`${pivotDate.getFullYear()}년 ${pivotDate.getMonth() + 1}월`}
+        leftChild={<Button text={'<'} onClick={onDecreaseMonth} />}
+        rightChild={<Button text={'>'} onClick={onIncreaseMonth} />}
       />
       <DiaryList />
     </div>
