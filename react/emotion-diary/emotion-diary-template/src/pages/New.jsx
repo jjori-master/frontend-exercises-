470ed736588018e4c0c5ee49e0c5a3c9b984612f
jjori <jjori.master@gmail.com>
2024-11-26 22:35:54 +0900

    components 폴더 앞부붙에 공백문자열 제거

--- react/emotion-diary/emotion-diary-template/src/pages/New.jsx
+++ react/emotion-diary/emotion-diary-template/src/pages/New.jsx
@@ -1,6 +1,6 @@
-import Header from '../ components/Header';
-import Button from '../ components/Button';
-import Editor from '../ components/Editor';
+import Header from '../components/Header';
+import Button from '../components/Button';
+import Editor from '../components/Editor';
 
 const New = () => {
   return (
