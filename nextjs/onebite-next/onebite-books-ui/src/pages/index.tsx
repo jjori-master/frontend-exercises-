4cbcd456d610b5572b7009ef3c63ebb90d74392a
jjori <jjori.master@gmail.com>
2024-12-08 23:44:48 +0900

    onebite nextjs 실습 예정 onebite-books ui 설정

---
+++ nextjs/onebite-next/onebite-books-ui/src/pages/index.tsx
@@ -0,0 +1,30 @@
+// CSS Module
+import SearchableLayout from '@/components/searchable-layout';
+import style from './index.module.css';
+import { ReactNode } from 'react';
+import BookItem from '@/components/book-item';
+
+import books from '@/mock/books.json';
+
+export default function Home() {
+  return (
+    <div className={style.container}>
+      <section>
+        <h3>지금 추천하는 도서</h3>
+        {books.map(book => {
+          return <BookItem key={book.id} {...book} />;
+        })}
+      </section>
+      <section>
+        <h3>등록된 모든 도서</h3>
+        {books.map(book => {
+          return <BookItem key={book.id} {...book} />;
+        })}
+      </section>
+    </div>
+  );
+}
+
+Home.getLayout = (page: ReactNode) => {
+  return <SearchableLayout>{page}</SearchableLayout>;
+};
