013ea64e2754467a46931c9fe3d7b221a5c6d54b
jjori <jjori.master@gmail.com>
2024-12-10 00:08:40 +0900

    추천 도서, 전체 도서 목록 SSR으로 가져오기

--- nextjs/onebite-next/onebite-books/src/pages/index.tsx
+++ nextjs/onebite-next/onebite-books/src/pages/index.tsx
@@ -4,20 +4,36 @@ import style from './index.module.css';
 import { ReactNode } from 'react';
 import BookItem from '@/components/book-item';
 
-import books from '@/mock/books.json';
+import { InferGetServerSidePropsType } from 'next';
+import fetchBooks from '@/lib/fetch-books';
+import fetchRandomBooks from '@/lib/fetch-random-books';
 
-export default function Home() {
+export const getServerSideProps = async () => {
+  const [allBooks, recoBooks] = await Promise.all([fetchBooks(), fetchRandomBooks()]);
+
+  return {
+    props: {
+      allBooks,
+      recoBooks,
+    },
+  };
+};
+
+export default function Home({
+  allBooks,
+  recoBooks,
+}: InferGetServerSidePropsType<typeof getServerSideProps>) {
   return (
     <div className={style.container}>
       <section>
         <h3>지금 추천하는 도서</h3>
-        {books.map(book => {
+        {recoBooks.map(book => {
           return <BookItem key={book.id} {...book} />;
         })}
       </section>
       <section>
         <h3>등록된 모든 도서</h3>
-        {books.map(book => {
+        {allBooks.map(book => {
           return <BookItem key={book.id} {...book} />;
         })}
       </section>
