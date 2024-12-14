512a29916a4e41af35b4391079fdba7af038f59f
jjori <jjori.master@gmail.com>
2024-12-14 10:48:48 +0900

    각 상황에 맞는 SEO 설정하기

--- nextjs/onebite-next/onebite-books/src/pages/index.tsx
+++ nextjs/onebite-next/onebite-books/src/pages/index.tsx
@@ -1,4 +1,3 @@
-// CSS Module
 import SearchableLayout from '@/components/searchable-layout';
 import style from './index.module.css';
 import { ReactNode } from 'react';
@@ -7,11 +6,10 @@ import BookItem from '@/components/book-item';
 import { InferGetStaticPropsType } from 'next';
 import fetchBooks from '@/lib/fetch-books';
 import fetchRandomBooks from '@/lib/fetch-random-books';
+import Head from 'next/head';
 
 export const getStaticProps = async () => {
   const [allBooks, recoBooks] = await Promise.all([fetchBooks(), fetchRandomBooks()]);
-
-  console.log('인덱스 페이지');
 
   return {
     props: {
@@ -26,20 +24,28 @@ export default function Home({
   recoBooks,
 }: InferGetStaticPropsType<typeof getStaticProps>) {
   return (
-    <div className={style.container}>
-      <section>
-        <h3>지금 추천하는 도서</h3>
-        {recoBooks.map(book => {
-          return <BookItem key={book.id} {...book} />;
-        })}
-      </section>
-      <section>
-        <h3>등록된 모든 도서</h3>
-        {allBooks.map(book => {
-          return <BookItem key={book.id} {...book} />;
-        })}
-      </section>
-    </div>
+    <>
+      <Head>
+        <title>한입 북스</title>
+        <meta property="og:image" content="/thumbnail.png" />
+        <meta property="og:title" content="한입북스" />
+        <meta property="og:description" content="한입 북스에 등록된 도서들을 만나보세요" />
+      </Head>
+      <div className={style.container}>
+        <section>
+          <h3>지금 추천하는 도서</h3>
+          {recoBooks.map(book => {
+            return <BookItem key={book.id} {...book} />;
+          })}
+        </section>
+        <section>
+          <h3>등록된 모든 도서</h3>
+          {allBooks.map(book => {
+            return <BookItem key={book.id} {...book} />;
+          })}
+        </section>
+      </div>
+    </>
   );
 }
 
